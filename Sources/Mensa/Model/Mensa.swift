a7adba23908ecf18d9d56e16291f80f84754fa5c
fwcd <fwcdmail@gmail.com>
2022-11-07 17:55:04 +0100

    Add some more fields to Meal

--- Sources/Mensa/Model/Mensa.swift
+++ Sources/Mensa/Model/Mensa.swift
@@ -4,11 +4,13 @@ public struct Meal: Codable, Identifiable, Hashable {
     public let name: String
     public let notes: [String]
     public let prices: Prices
+    public let category: String?
 
     /// The prices for a meal.
     public struct Prices: Codable, Hashable {
         public let students: Double?
         public let employees: Double?
+        public let pupils: Double?
         public let others: Double?
     }
 }
